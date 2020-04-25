50d1e4931bdd6fa562030b448157f35c4df23c1b
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-04-25 22:39:10 +0400

    add align r/t

--- src/main/java/com/kolll/Launch.java
+++ src/main/java/com/kolll/Launch.java
@@ -5,12 +5,16 @@ import com.kolll.datafetch.model.CovidDataModel;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.awt.*;
 
 public class Launch extends Application {
     @Override
@@ -27,6 +31,11 @@ public class Launch extends Application {
         Scene scene = new Scene (root);
         secondaryStage.setScene(scene);
         secondaryStage.show();
+
+//        aligned right-top
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        secondaryStage.setX(visualBounds.getMaxX() - 25 - scene.getWidth());
+        secondaryStage.setY(visualBounds.getMinY() + 25);
 //
 //        CovidDataModel dataModel = new DataProviderService().getData("Russia");
 //        System.out.println(dataModel.toString());
