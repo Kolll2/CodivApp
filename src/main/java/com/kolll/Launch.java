8f70a674ab0f8913a7d9b47c9f587bd082dc3f28
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2020-04-25 22:31:04 +0400

    update window format

--- src/main/java/com/kolll/Launch.java
+++ src/main/java/com/kolll/Launch.java
@@ -10,17 +10,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Launch extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/kolll/gui/widget/widget.fxml"));
-
-        Scene scene = new Scene (root);
-        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setOpacity(0);
         primaryStage.show();
 
-        CovidDataModel dataModel = new DataProviderService().getData("Russia");
-        System.out.println(dataModel.toString());
+        Stage secondaryStage = new Stage();
+        secondaryStage.initStyle(StageStyle.UNDECORATED);
+        secondaryStage.initOwner(primaryStage);
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kolll/gui/widget/widget.fxml"));
+        Scene scene = new Scene (root);
+        secondaryStage.setScene(scene);
+        secondaryStage.show();
+//
+//        CovidDataModel dataModel = new DataProviderService().getData("Russia");
+//        System.out.println(dataModel.toString());
     }
 }
